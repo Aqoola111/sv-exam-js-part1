6d261af34c3ee6b295400cfdadfe6cf11e31ae9a
David Tananashvili <datunebi2003@gmail.com>
2026-06-07 17:36:01 +0300

    Fixed bug where if the arr[i+1] is not a number the loop goes on. added the question so i will be able to solve that at home

--- ex1.js
+++ ex1.js
@@ -2,7 +2,7 @@ const checkSequenceCount = (arr, num) => {
     let result = 0;
     let sum = 0;
     for(let i = 0; i<arr.length-1; i++){
-        if (typeof arr[i] !== 'number') throw new Error(`${arr[i]} not a number`)
+        if (typeof arr[i] !== 'number' || typeof arr[i+1] !== 'number') throw new Error(`Not a number`)
         sum = arr[i];
         for(let x = i+1; x<arr.length; x++){
             // The sum will be equal or not equal to num only after addition so it guaranties that there are 2 numbers in a sequence
